2c585dcda8612b08e1b1a13a300b23ccdb120a89
Artemkazak-tech <cazack161@gmail.com>
2025-03-12 18:03:40 +0300

    Add files via upload

---
+++ 10.3.3.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+#include <iterator> // для std::size
+
+int main()
+{
+    constexpr int scores[]{ 84, 92, 76, 81, 56 };
+
+    int maxScore{ 0 }; // Для начала предположим, что наш наибольший результат равен 0
+    int maxIndex{ 0 };
+    // теперь ищем максимальный результат во всем массиве
+    for (int student{ 0 }; student < std::size(scores); ++student)
+    {
+        if (scores[student] > maxScore)
+        {
+            maxScore = scores[student];
+            maxIndex = student;
+          
+            /*std::cout << "The best score was " << maxScore << student << '\n';*/
+        }
+      
+    }
+    std::cout << "The best score was " << maxScore << " has index " << maxIndex << '\n';
+
+    return 0;
+}
+
+
+
+
